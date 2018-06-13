66cb9b463936f7a88f251d7598e905531c66d1e8
Sin-Han,Jhu <blueice0916@gmail.com>
2018-06-13 22:49:22 +0800

    header CSS新增 -logo圖片  margin屬性 -searchbar 屬性

--- week2/src/containers/header/header.js
+++ week2/src/containers/header/header.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './header.css';
-import logo from '../../asset/logo@2x.png'
+import logo from '../../asset/logo.png'
 
 class Header extends Component {
     render(){
         return(
             <div className="Header">
-                <img src={logo} alt="logo"/>
-                <div>
+                <img className="Logo" src={logo} alt="logo"/>
+                <div className="SearchLine" >
                     <i className="fas fa-search"></i>
                     <input className="HeaderSearch" type="text" placeholder="Explore your own activities"/>
                 </div>
